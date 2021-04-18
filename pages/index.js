d715688fe24d80899805fc9e1eb21852e0d1ef16
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-04-18 17:17:13 +0100

    done with the nav-links

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,18 @@ import Head from "next/head";
 import Avatar from "../components/Avatar";
 import Image from "next/image";
 import { MenuIcon } from "@heroicons/react/solid";
+import { useState } from "react";
 
 export default function Home() {
+  const [showNav, setShowNav] = useState(true);
+  const links = [
+    "courses",
+    "Road-Map",
+    "Resourses",
+    "Design Trends",
+    "Jobs",
+    "Pricing",
+  ];
   return (
     <div className="">
       <Head>
@@ -12,7 +22,7 @@ export default function Home() {
       </Head>
 
       <div className="h-screen sm:h-[70vh] w-full text-white relative bg-gradient-to-r from-[#03056b] via-[#7703ef] to-[#35d4fb]">
-        <div className="flex px-5 py-2 items-center w-full border-b-2 border-[#35d4fb]">
+        <div className="flex px-5 py-2 items-center overflow-hidden w-full border-b-2 border-[#35d4fb]">
           <MenuIcon className="h-8 mr-1 sm:hidden" />
           <Image
             src="/images/lightlogo.png"
@@ -20,10 +30,20 @@ export default function Home() {
             height={30}
             width={200}
           />
-          <div className="h-[90%] w-full absolute top-[3.75rem] left-0 text-w bg-[#35d4fb] flex flex-col">
-            <p>the terms</p>
-            <p>the terms</p>
-            <p>the terms</p>
+          <div
+            className={`h-[90%] w-full absolute top-[3.75rem] ${
+              showNav ? "left-0" : "left-[-100%] "
+            } text-w bg-[#35d4fb] flex flex-col items-center 
+            justify-evenly text-2xl font-semibold 
+            sm:flex-row sm:text-base sm:bg-[transparent]
+            sm:flex-grow
+            sm:static`}
+          >
+            {links.map((link) => (
+              <h3 className="p-2 border-b-2 border-transparent hover:border-[#03056b] cursor-pointer">
+                {link}
+              </h3>
+            ))}
           </div>
           <Avatar
             url="https://avatars.githubusercontent.com/u/69096827?v=4"
