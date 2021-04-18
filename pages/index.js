ce7d5291eda2c80076088f4d96c3a7f3e4ebef8e
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-04-18 06:41:57 +0100

    added the tailwind configuration

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,21 @@
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
       <Head>
         <title>QuiskStack</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/logos.png" />
       </Head>
 
-      <div className="">
+      <div className="text-gray-200 ">
+        <Image
+          src="/images/lightlogo.png"
+          className="bg-blue-900 "
+          height={50}
+          width={300}
+        />
         <h2>this is the start of the const</h2>
       </div>
     </div>
