71cd5f7465539a1f2edefa014b08fdfd5f49b497
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-04-18 15:04:17 +0100

    working on the side nav

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,11 +1,9 @@
-import Image from "next/image";
-
 function Avatar({ url, className }) {
   return (
-    <Image
+    <img
       src={url}
       loading="lazy"
-      className={`h-10 rounded-full animate-bounce cursor-pointer transition duration-150 transform hover:scale-110 ${className} `}
+      className={`h-9 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className} `}
       alt="profile pic"
     />
   );
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
 import Head from "next/head";
+import Avatar from "../components/Avatar";
 import Image from "next/image";
-import {
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  MenuIcon,
-} from "@heroicons/react/solid";
+import { MenuIcon } from "@heroicons/react/solid";
 
 export default function Home() {
   return (
@@ -14,14 +11,23 @@ export default function Home() {
         <link rel="icon" href="/images/logos.png" />
       </Head>
 
-      <div className="h-screen sm:h-[70vh] w-full text-white  bg-gradient-to-r from-[#03056b] via-[#7703ef] to-[#35d4fb]">
-        <div className="flex p-5 items-center w-full">
-          <MenuIcon className="h-10 mr-5" />
+      <div className="h-screen sm:h-[70vh] w-full text-white relative bg-gradient-to-r from-[#03056b] via-[#7703ef] to-[#35d4fb]">
+        <div className="flex px-5 py-2 items-center w-full border-b-2 border-[#35d4fb]">
+          <MenuIcon className="h-8 mr-1 sm:hidden" />
           <Image
             src="/images/lightlogo.png"
-            className=""
-            height={40}
+            className="mr-2"
+            height={30}
             width={200}
+          />
+          <div className="h-[90%] w-full absolute top-[3.75rem] left-0 text-w bg-[#35d4fb] flex flex-col">
+            <p>the terms</p>
+            <p>the terms</p>
+            <p>the terms</p>
+          </div>
+          <Avatar
+            url="https://avatars.githubusercontent.com/u/69096827?v=4"
+            className="ml-auto"
           />
         </div>
       </div>
