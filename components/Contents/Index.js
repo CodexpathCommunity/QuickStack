671b2ff0b7cb8d41692c1b27d427feff267f0d63
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-04-19 09:47:19 +0100

    added the search function

--- components/Contents/Index.js
+++ components/Contents/Index.js
@@ -102,9 +102,9 @@ function Contents() {
 
   return (
     <div className="flex flex-col w-full max-w-screen-xl m-auto ">
-      <div className="flex items-center justify-between">
+      <div className="flex flex-col sm:flex-row sm:items-center justify-between p-2">
         <div className="flex items-center space-x-2 ">
-          {linkData.map(({ state, name, value }) => (
+          {lineState.map(({ state, name, value }) => (
             <Link
               active={state}
               name={name}
@@ -113,25 +113,31 @@ function Contents() {
             />
           ))}
         </div>
-        <form className="flex flex-grow border px-6 py-3 ml-10 mr-5 border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
-          <input
-            type="text"
-            ref={searchInputRef}
-            className="flex-grow w-full focus:outline-none"
-            defaultValue={router.query.term}
-          />
-          <XIcon
-            className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
-            onClick={() => (searchInputRef.current.value = "")}
-          />
-          <MicrophoneIcon className="mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300" />
-          <SearchIcon
-            className="h-6 hidden sm:inline-flex text-blue-500 cursor-pointer"
-            onClick={search}
-          />
+        {linkState.search && (
+          <form
+            className="
+        flex flex-grow border px-6 py-2 
+         sm:m-auto border-gray-200 rounded-full shadow-lg max-w-3xl items-center mt-2"
+          >
+            <input
+              type="text"
+              ref={searchInputRef}
+              className="flex-grow w-full focus:outline-none"
+              defaultValue={router.query.term}
+            />
+            <XIcon
+              className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
+              onClick={() => (searchInputRef.current.value = "")}
+            />
+            <MicrophoneIcon className="mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300" />
+            <SearchIcon
+              className="h-6 hidden sm:inline-flex text-blue-500 cursor-pointer"
+              onClick={search}
+            />
 
-          <button hidden type="submit" onClick={search}></button>
-        </form>
+            <button hidden type="submit" onClick={search}></button>
+          </form>
+        )}
       </div>
     </div>
   );
