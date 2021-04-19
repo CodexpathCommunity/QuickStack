c85c48c49e69fb053ee2eeb078bad8ec2ad515e2
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-04-19 09:30:38 +0100

    added the search field

--- components/Contents/Index.js
+++ components/Contents/Index.js
@@ -1,7 +1,19 @@
 import { useState } from "react";
 import Link from "./Link";
+import { MicrophoneIcon, SearchIcon, XIcon } from "@heroicons/react/solid";
+import { useRef } from "react";
+import { useRouter } from "next/router";
 
 function Contents() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+    if (!term) return;
+    router.push(`/search?term=${term}`);
+  };
   const [lineState, setLineState] = useState({
     search: false,
     popular: true,
@@ -90,15 +102,36 @@ function Contents() {
 
   return (
     <div className="flex flex-col w-full max-w-screen-xl m-auto ">
-      <div className="flex items-center space-x-2 ">
-        {linkData.map(({ state, name, value }) => (
-          <Link
-            active={state}
-            name={name}
-            value={value}
-            onClick={handleClick}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center space-x-2 ">
+          {linkData.map(({ state, name, value }) => (
+            <Link
+              active={state}
+              name={name}
+              value={value}
+              onClick={handleClick}
+            />
+          ))}
+        </div>
+        <form className="flex flex-grow border px-6 py-3 ml-10 mr-5 border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
+          <input
+            type="text"
+            ref={searchInputRef}
+            className="flex-grow w-full focus:outline-none"
+            defaultValue={router.query.term}
           />
-        ))}
+          <XIcon
+            className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
+            onClick={() => (searchInputRef.current.value = "")}
+          />
+          <MicrophoneIcon className="mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300" />
+          <SearchIcon
+            className="h-6 hidden sm:inline-flex text-blue-500 cursor-pointer"
+            onClick={search}
+          />
+
+          <button hidden type="submit" onClick={search}></button>
+        </form>
       </div>
     </div>
   );
