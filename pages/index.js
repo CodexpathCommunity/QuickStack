8c5236ba9ae04029fcabde1df60a65cbe5301479
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-04-19 06:47:01 +0100

    started the contents section

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import { useState } from "react";
 import Header from "../components/Header";
 import SideNav from "../components/SideNav";
 import Hero from "../components/Hero";
+import Contents from "../components/Contents";
 
 export default function Home() {
   const [showNav, setShowNav] = useState(false);
@@ -26,8 +27,8 @@ export default function Home() {
 
       <Header showNav={showNav} setShowNav={setShowNav} links={links} />
       <SideNav showNav={showNav} setShowNav={setShowNav} links={links} />
-
       <Hero />
+      <Contents />
     </div>
   );
 }
