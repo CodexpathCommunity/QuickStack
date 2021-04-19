8205f604570bc4cfdb1e1d78cadc9d87a926a90b
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-04-19 14:04:16 +0100

    perfected the card

--- components/Card.js
+++ components/Card.js
@@ -4,13 +4,35 @@ import Avatar from "./Avatar";
 
 function Card() {
   return (
-    <div className="flex flex-col p-4 items-start  w-full max-w-xs bg-[#35d4fb] m-auto rounded-sm shadow-xl">
-      <Avatar url="/public/vercel.svg" className="mb-4" />
-      <h2>Amazin Forks WlasticSeard Rebrandin its open search</h2>
-      <h3>Today 3m read timw</h3>
-      <Image src="/images/learncode.svg" height={40} width={30} />
-      <ArrowCircleUpIcon />
-      <ChatIcon />
+    <div
+      className="
+      cursor-pointer
+    flex flex-col border-2 border-transparent hover:border-[#03056b] p-4 
+    items-start text-white w-full 
+    max-w-xs bg-[#7703ef] m-auto 
+    rounded-md shadow-md hover:shadow-lg
+    "
+    >
+      <Avatar url="/images/logos.png" className="mb-3" />
+      <h2 className="text-xl font-lg mb-1">
+        Amazin Forks WlasticSeard Rebrandin its open search
+      </h2>
+      <h3 className="text-xs font-md">Today 3m read timw</h3>
+      <Image
+        src="/images/learncode.svg"
+        height={400}
+        width={500}
+        className="w-full"
+      />
+      <div className="flex items-start space-x-8">
+        <div className="flex items-center space-x-2">
+          <ArrowCircleUpIcon className="h-5" />{" "}
+          <h2 className="text-sm font-lg">1</h2>
+        </div>
+        <div className="flex items-center space-x-2">
+          <ChatIcon className="h-4" /> <h2 className="text-sm font-lg">4</h2>
+        </div>
+      </div>
     </div>
   );
 }
