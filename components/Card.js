f97d2ec6ef707061b04688908f5cf54cf4894b00
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-04-19 12:08:08 +0100

    working on the grid cards

--- components/Card.js
+++ components/Card.js
@@ -5,17 +5,10 @@ import Avatar from "./Avatar";
 function Card() {
   return (
     <div className="flex flex-col p-4 items-start  w-full max-w-xs bg-[#35d4fb] m-auto rounded-sm shadow-xl">
-      <Avatar
-        url="https://avatars.githubusercontent.com/u/69096827?v=4"
-        className="mb-4"
-      />
+      <Avatar url="/public/vercel.svg" className="mb-4" />
       <h2>Amazin Forks WlasticSeard Rebrandin its open search</h2>
       <h3>Today 3m read timw</h3>
-      <Image
-        src="https://avatars.githubusercontent.com/u/69096827?v=4"
-        height={40}
-        width={30}
-      />
+      <Image src="/images/learncode.svg" height={40} width={30} />
       <ArrowCircleUpIcon />
       <ChatIcon />
     </div>
