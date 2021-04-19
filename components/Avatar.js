636fd3be460ff795d0c540ac0bd7366cdf37b5c9
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-04-19 03:35:25 +0100

    workked on the sidenav

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,6 +1,7 @@
-function Avatar({ url, className }) {
+function Avatar({ url, className, setShowNav }) {
   return (
     <img
+      onClick={() => setShowNav(false)}
       src={url}
       loading="lazy"
       className={`h-9 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className} `}
