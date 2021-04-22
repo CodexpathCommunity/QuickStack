16de33025fb84fa6e1f4b51f11346eb505d4b611
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-04-22 23:45:38 +0100

    implemented the firebase upload system

--- components/Upload.js
+++ components/Upload.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { db, storage } from "../firebase";
+import firebase from "firebase";
 
 function Upload() {
   const [thumbnail, setThumbnail] = useState(null);
@@ -6,6 +8,45 @@ function Upload() {
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
   const [sucess, setSucess] = useState(false);
+
+  const handleImgChange = (e) => {
+    if (e.target.files[0]) {
+      setThumbnail(e.target.files[0]);
+    }
+  };
+  const handleVidChange = (e) => {
+    if (e.target.files[0]) {
+      setVideo(e.target.files[0]);
+    }
+  };
+
+  const handleUpload = () => {
+    storage
+      .ref(`courses/${title}`)
+      .put(thumbnail)
+      .put(video)
+      .then((snapshot) => {
+        storage
+          .ref("courses")
+          .child(title)
+          .getDownloadURL()
+          .then((url) => {
+            db.collection("All_COURSES").add({
+              timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+              title: title,
+              thumbnail: thumbnail,
+              video: video,
+              imageUrl: url,
+              description: desc,
+            });
+            setSucess(true);
+            setThumbnail(null);
+            setTitle("");
+            setDesc("");
+          });
+      });
+  };
+
   return (
     <div className="flex items-center justify-evenly h-screen flex-col">
       <h2
@@ -31,6 +72,9 @@ function Upload() {
           <h2 className="text-xl font-bold">Title :</h2>
           <input
             type="text"
+            placeholder="Name of Content"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             className="p-2 border-2 w-full rounded-md border-[#03056b] focus:outline-none "
           />
           <h2 className="text-xl font-bold">Thumbnail :</h2>
@@ -38,16 +82,28 @@ function Upload() {
             type="file"
             name="myImage"
             accept="image/x-png,image/gif,image/jpeg"
+            onChange={handleImgChange}
             className="p-2 border-2 w-full rounded-md border-[#03056b] focus:outline-none "
           />
           <h2 className="text-xl font-bold">Video :</h2>
           <input
             type="file"
             accept="video/mp4,video/x-m4v,video/*"
+            onChange={handleVidChange}
             className="p-2 border-2 w-full rounded-md border-[#03056b] focus:outline-none "
           />
           <h2 className="text-xl font-bold">Description :</h2>
-          <textarea className="p-2 border-2 w-full rounded-md border-[#03056b] focus:outline-none resize-none h-36" />
+          <textarea
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+            className="
+            p-2 border-2 w-full 
+            rounded-md border-[#03056b] 
+            focus:outline-none resize-none h-36"
+          />
+          <button onClick={handleUpload} className="btn w-3/4 m-auto text-2xl">
+            Upload
+          </button>
         </form>
       </div>
     </div>
